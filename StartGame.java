acc361f1825ed91fbce8fced11e47d28fa23915c
Sandeep Singh <sandeepgaurdec13@gmail.com>
2020-12-29 21:43:30 +0530

    Start Game With player 1 and position 0

---
+++ StartGame.java
@@ -0,0 +1,14 @@
+/**
+ * @author Sandeep_Singh
+ * @Functionality Snake and Ladder game played with single player at start position 0
+ *
+ */
+public class StartGame 
+{
+    private static final int player=1;
+    private static int position=0;
+	public static void main( String[] args )
+    {
+        System.out.println( "Let's start the Game with player and position is"+" "+player+" "+"and"+" "+position);
+    }
+}
